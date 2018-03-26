3a9e597e50bde6dcaed4a978dccba15c362a9c63
joglatt <34425753+joglatt@users.noreply.github.com>
2018-03-26 07:32:49 -0400

    add

--- trivia.js
+++ trivia.js
@@ -1,17 +1,45 @@
-//Questions constructor function
+//Questions constructor 
 function questions(quest, a1, a2, a3, a4, correct) {
-    this.quest = quest;
-    this.a1 = a1;
-    this.a2 = a2;
-    this.a3 = a3;
-    this.a4 = a4;
-    this.correct = correct;
+  this.quest = quest;
+  this.a1 = a1;
+  this.a2 = a2;
+  this.a3 = a3;
+  this.a4 = a4;
+  this.correct = correct;
 }
 //questions
-var one = new questions("Where is A located in the alphabet?", "1st", "2nd", "3rd", "4th", "1st");
-var two = new questions("Where is B located in the alphabet?", "1st", "2nd", "3rd", "4th", "2nd");
-var three = new questions("Where is C located in the alphabet?", "1st", "2nd", "3rd", "4th", "3rd");
-var four = new questions("Where is D located in the alphabet?", "1st", "2nd", "3rd", "4th", "4th");
+var one = new questions(
+  "Where is A located in the alphabet?",
+  "1st",
+  "2nd",
+  "3rd",
+  "4th",
+  "1st"
+);
+var two = new questions(
+  "Where is B located in the alphabet?",
+  "1st",
+  "2nd",
+  "3rd",
+  "4th",
+  "2nd"
+);
+var three = new questions(
+  "Where is C located in the alphabet?",
+  "1st",
+  "2nd",
+  "3rd",
+  "4th",
+  "3rd"
+);
+var four = new questions(
+  "Where is D located in the alphabet?",
+  "1st",
+  "2nd",
+  "3rd",
+  "4th",
+  "4th"
+);
 //array of questions
 var questionArray = [one, two, three, four];
 //time value
@@ -22,38 +50,65 @@ var score = 0;
 var intervalId;
 //user guess
 var guess;
+//correct answer
+var correct;
+//question currently dispalyed
+var displayQuestion;
+//used questions
+var usedQuestions = [];
 
-function game() {
-    number = 10
-    clearInterval(intervalId);
-    intervalId = setInterval(deincrement, 1000);
-    $("#timer").html("<h2>Time: " + number + "</h2>");
-    //loops through q/a array and displays
-    for (i = 0; i < questionArray.length; i++) {
-        $(".jumbotron").html("<h1>" + questionArray[i].quest + "<h1>");
-        $("#answer-1").html(questionArray[i].a1);
-        $("#answer-2").html(questionArray[i].a2);
-        $("#answer-3").html(questionArray[i].a3);
-        $("#answer-4").html(questionArray[i].a4);
+function click() {
+  $(".answer").click(function() {
+    guess = $(this).text();
+    console.log(guess);
+    if (guess == correct) {
+      score++;
+      console.log(score);
+      console.log("correct");
+      clock();
+      display();
+    } else if (guess != correct) {
+      console.log("wrong");
+      display();
+      clock();
     }
+  });
+}
+
+function display() {
+  displayQuestion =
+    questionArray[Math.floor(Math.random() * questionArray.length)];
+  usedQuestions.push(displayQuestion);
+  
+    $(".jumbotron").html("<h1>" + displayQuestion.quest + "<h1>");
+    $("#answer-1").html("<button>" + displayQuestion.a1 + "</button>");
+    $("#answer-2").html("<button>" + displayQuestion.a2 + "</button>");
+    $("#answer-3").html("<button>" + displayQuestion.a3 + "</button>");
+    $("#answer-4").html("<button>" + displayQuestion.a4 + "</button>");
+    correct = displayQuestion.correct;
+  
+}
+function clock() {
+  number = 10;
+  clearInterval(intervalId);
+  intervalId = setInterval(countDown, 1000);
+  $("#timer").html("<h2>Time: " + number + "</h2>");
 }
 //lowers timer
-function deincrement() {
-    number--;
-    if (number < 0) {
-        game();
-    }
+function countDown() {
+  number--;
+  $("#timer").html("<h2>Time: " + number + "</h2>");
+  //resets timer
+  if (number < 0) {
+    clock();
+    display();
+  }
 }
 
-$(document).ready(function () {
-    prompt("Play the game stupid.")
-    game();
-    $(".answers").on("click", ".answer", function () {
-        guess = $(this).text();
-        if (guess === ) {
-            score++;
-            game();
-        }
-    });
+$(document).ready(function() {
+  // prompt("Play the game stupid.")
+  clock();
+  display();
+  click();
 
 });
