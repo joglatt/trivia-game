3e94ddcd0a1283d79c8b149d5dfceb5b724ef07b
joglatt <34425753+joglatt@users.noreply.github.com>
2018-03-27 17:50:23 -0400

    add

--- trivia.js
+++ trivia.js
@@ -9,12 +9,12 @@ function questions(quest, a1, a2, a3, a4, correct) {
 }
 //questions
 var one = new questions(
-  "If I had a hammer?",
-  "I'd Hammer in the Morning",
-  "I'd Hammer in the Evening",
-  "I'd Hammer out Danger",
-  "I'd Hammer out Warning",
-  "I'd Hammer in the Evening"
+  "Where is A located in the alphabet?",
+  "1st",
+  "2nd",
+  "3rd",
+  "4th",
+  "1st"
 );
 var two = new questions(
   "Where is B located in the alphabet?",
@@ -46,6 +46,8 @@ var questionArray = [one, two, three, four];
 var number = 10;
 //questions guessed correctly
 var score = 0;
+//questions guessed incorrectly
+var wrong = 0;
 //timer variable
 var intervalId;
 //user guess
@@ -55,7 +57,7 @@ var correct;
 //question currently dispalyed
 var displayQuestion;
 //used questions
-var usedQuestions = [];
+var qCount = 0;
 //guess and score logic
 function click() {
   $(".answer").click(function() {
@@ -65,30 +67,43 @@ function click() {
       score++;
       console.log(score);
       console.log("correct");
+      qCount++;
       clock();
       display();
     } else if (guess != correct) {
+      wrong++;
       console.log("wrong");
-      alert("The Correct Answer was: " + displayQuestion.correct );
+      alert("The Correct Answer was: " + displayQuestion.correct);
+      qCount++;
       display();
       clock();
     }
   });
 }
+//random questions
+// questionArray[Math.floor(Math.random() * questionArray.length)];
+// if (usedQuestions.indexOf(displayQuestion) === -1) {
+//   variables();}
+
+  //pop off last question 
+  // displayQuestion = questionArray[questionArray.length - 1];
+  // variables();
+  // questionArray.splice((questionArray.length - 1), 1);
 
 function display() {
-  displayQuestion =
-    questionArray[Math.floor(Math.random() * questionArray.length)];
-  if (usedQuestions.indexOf(displayQuestion) === -1) {
+//loops through questions and takes the index equal to the count
+  for (i = 0; i < questionArray.length; i++) {
+    displayQuestion = questionArray[qCount];
+  }
+  //if count is euqla to the length of the array, end game
+  if (qCount === questionArray.length) {
+    clearInterval(intervalId);
+    $(".jumbotron").html("<h1>You answered " + score + " questions correctly<h1>");
+    $("#timer").html("<h2>You got " + wrong + " incorrect<h2>");
+    $(".answers").html('<img src="assets/images/stupid-idiot.jpg" />');
+  }else{
     variables();
-    usedQuestions.push(displayQuestion);
-    
-    if (usedQuestions.length === questionArray.length) {
-      clearInterval(intervalId);
-      alert("Game Over dummy");
-      $(".jumbotron").html("<h1>You answered " + score + " questions correctly<h1>");
-      $(".answers").html('<img src="assets/images/stupid-idiot.jpg" />');
-    }
+
   }
 }
 //pairs variables to html elements
@@ -113,6 +128,8 @@ function countDown() {
   $("#timer").html("<h2>Time: " + number + "</h2>");
   //resets timer
   if (number < 0) {
+    qCount++;
+    wrong++;
     clock();
     display();
   }
